0bf225a17faf97d9789846dde41ba0627307e757
yoni <yoni.amarv@gmail.com>
2018-12-11 18:05:14 +0200

    add function maillot

--- shop_project/shop_app/admin.py
+++ shop_project/shop_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from shop_app.models import Product, Customer
+from shop_app.models import Product, Customer, Maillot
 
 admin.site.register(Product)
 admin.site.register(Customer)
+admin.site.register(Maillot)
--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
-from shop_app.models import Product, Customer
+from shop_app.models import Product, Customer, Maillot
 
 
 def index(request):
-  products = Product.objects.all()[:20]
-  return render(request, 'index.html', context={ 'products': products })
+  	products = Product.objects.all()[:20]
+  	return render(request, 'index.html', context={ 'products': products })
 
 
 def product(request, product_id):
-  product = Product.objects.get(id=product_id)
-  return render(request, 'product.html', context={ 'product': product })
+  	product = Product.objects.get(id=product_id)
+  	return render(request, 'product.html', context={ 'product': product })
 
 def customers(request):
-  customers = Customer.objects.all()[:20]
-  return render(request, 'customers.html', context={ 'customers': customers })
+  	customers = Customer.objects.all()[:20]
+  	return render(request, 'customers.html', context={ 'customers': customers })
 
 def customer(request, customer_id):
 	customer = Customer.objects.get(id=customer_id)
 	return render(request, 'customer.html', context={ 'customer': customer })
+
+def maillots(request):
+  	maillots = Maillot.objects.all()
+  	return render(request, 'maillots.html', context={ 'maillots': maillots })
+
+def maillot(request, maillot_id):
+  	maillot = Maillot.objects.get(id=maillot_id)
+  	return render(request, 'maillot.html', context={ 'maillot': maillot })
